6708597cbaed568521b1f72ec36d7c31a4d9dc90
Amir <39675277+MAmir20@users.noreply.github.com>
2024-01-15 17:04:40 +0100

    removed order service

--- order-service/src/main/java/tn/enis/userservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.enis.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.enis.userservice.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
