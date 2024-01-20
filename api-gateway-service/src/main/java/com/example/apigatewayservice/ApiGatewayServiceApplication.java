54a58eeeb01786a79b36b1f2d542c7324637ce6c
AkroutHassen <hassenakrout5@gmail.com>
2024-01-20 18:30:47 +0100

    front end template added

--- api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,12 +2,17 @@ package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
 @SpringBootApplication
+//@CrossOrigin(origins = "http://localhost:3000")
 public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
--- user-service/src/main/java/tn/enis/userservice/UserServiceApplication.java
+++ user-service/src/main/java/tn/enis/userservice/UserServiceApplication.java
@@ -4,12 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import tn.enis.userservice.model.User;
 import tn.enis.userservice.repository.UserRepository;
 
 import java.util.List;
 
 @SpringBootApplication
+@EnableWebMvc
 public class UserServiceApplication implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
@@ -24,5 +30,19 @@ public class UserServiceApplication implements CommandLineRunner {
 		userRepository.save(new User(null, "user1", "user1@gmail.com", "1234"));
 		userRepository.save(new User(null, "user2", "user2@gmail.com", "1234"));
 		userRepository.save(new User(null, "user3", "user3@gmail.com", "1234"));
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				//WebMvcConfigurer.super.addCorsMappings(registry);
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
